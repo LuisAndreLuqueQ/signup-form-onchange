41275aa5f827d0baa87804ad850c5e230ea21cae
LuisAndreLuqueQ <l.luque.work@gmail.com>
2026-06-17 17:21:35 -0500

    Configuración de Tailwind y prueba exitosa

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600 underline">
+  ¡Tailwind está funcionando!
+</h1>
+    </>
+  )
+}
+
+export default App
